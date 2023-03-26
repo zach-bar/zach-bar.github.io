bc1598682ad63fca51048b867886a66f43550d7f
zach-bar <zachbar26@gmail.com>
2023-03-25 22:38:38 -0700

    spicy blog changes

--- js/blogscript.js
+++ js/blogscript.js
@@ -38,7 +38,7 @@ blogs = [
       image: "./assets/chatgpt.webp",
       date: "March 30, 2023",
       type: "Tech Thoughts",
-      post: "With the advent of Artificial Intelligence officially under way, showing itself to the general public through chatbots now developed by Microsoft, Google, and Apple, it is important to acknowledge the forefather of this new awakening. ChatGPT, a language model created by OpenAI to understand and generate natural language. A computer program that is designed to simulate human conversation and provide helpful responses to a wide range of queries and topics, ChatGPT is capable of processing large amounts of data, analyzing complex patterns, and generating accurate and relevant responses in real-time. Its abilities include understanding various languages, conducting research, providing recommendations, and performing various tasks like scheduling appointments, setting reminders, and answering general knowledge questions. It can even generate a description of itself, which I surely did NOT do. <br> <img src='./assets/chatgptss.png' width:70% height: 50%> <br> As you can see, I totally lied. The description it gave me, I copied word for word, only leaving out a few unnecessary adages and making some structural changes. With this in mind, I think it'll be pretty easy to detect that its AI-written, right? <br> <img src='./assets/gptzeross.png' width:70% height: 70%> <br> <h2>Wrong!</h2> <br> And this is totally understandable. At the end of the day, all Machine Learning comes down to is a set of algorithms that can learn from data, identify patterns, and make predictions or decisions based on those patterns. With systems built on this, it is very difficult to predict truly what is and what isn't artificially crafted. Even though this is all true, as my above experiment has proven (which I also got on my first attempt), academic instructors across the globe have taken it into their own hands to hold their students accountable over the misleading and uninformed results of a website with lack of verifiable credit. Not only has this epidemic undermined the integrity of students, but it has also allowed professors to become more hyper-vigilant and presumptuous in addressing plagiarism.",
+      post: "<span class='indent'>With the advent of Artificial Intelligence officially under way, showing itself to the general public through chatbots now developed by Microsoft, Google, and Apple, it is important to acknowledge the forefather of this new awakening: ChatGPT, a language model created by OpenAI to understand and generate natural language. A computer program that is designed to simulate human conversation and provide helpful responses to a wide range of queries and topics, ChatGPT is capable of processing large amounts of data, analyzing complex patterns, and generating accurate and relevant responses in real-time. Its abilities include understanding various languages, conducting research, providing recommendations, and performing various tasks like scheduling appointments, setting reminders, and answering general knowledge questions. It can even generate a description of itself, which I surely did NOT do.</span> <br> <img src='./assets/chatgptss.png' width:70% height: 50%> <br> <span class='indent'>As you can see, I totally lied. The description it gave me, I copied word for word, only leaving out a few unnecessary adages and making some structural changes. With this in mind, I think it'll be pretty easy to detect that its AI-written, right?</span> <br> <img src='./assets/gptzeross.png' width:70% height: 70%> <br> <h2>Wrong!</h2> <br> <span  class='indent'>And this is totally understandable. At the end of the day, all Machine Learning comes down to is a set of algorithms that can learn from data, identify patterns, and make predictions or decisions based on those patterns. With systems built on this, it is very difficult to predict truly what is and what isn't artificially crafted. Even though this is all true, as my above experiment has proven (which I got on my first attempt), academic instructors across the globe have taken it into their own hands to hold their students accountable over the misleading and uninformed results of a website with lack of verifiable credit. Not only has this epidemic undermined the integrity of students, but it has also allowed professors to become more hyper-vigilant and presumptuous in addressing plagiarism. </span> <br> <span class='indent'>This series of unfortunate events has characterized the scholars of our generation. The spoiling of the youth by the enrichment of technology and the growth of online resources has plagued the minds of previous generations. Alumni envy students now more than ever, but the truth is that it has actually left us paranoid, distraught, and cutthroat, fretful both of having our careers dismantled by AI and meeting growing expectations every new year. The students being accused are regularly high achievers assumed to not be capable of their achievements, resulting with those who try the hardest suffering the most.</span> <br> <span  class='indent'>The truth of the matter is, technology is growing, and we can either grow with it or let it keep us from growing. With everything being so new, it'd be unlikely for anyone, even an expert in AI, to be able to tell the difference. It is up to us to decide whether we'd like to hold onto our integrity or use the resources bestowed upon us. Either way, we're screwed. </span> <br> <i> Zach Bar </i> ",
     },
     {
       title: "How To Make Your Own JavaScript Game",
@@ -52,19 +52,34 @@ blogs = [
   let blogColumn = document.getElementById('blogs-column');
   for (let i = blogs.length-1; i >= 0; i--){
     let blog = document.createElement('div');
-    blog.onclick = function(){showBlog(i)};
     blog.classList.add('blog');
     blog.id = 'blog' + i;
     blogColumn.appendChild(blog);
+    let click = document.createElement('div');
+    click.addEventListener("click", function(){showBlog(i)});
+    blog.appendChild(click);
     let blogImg = document.createElement('img');
     blogImg.src=blogs[i].image;
-    blog.appendChild(blogImg);
+    click.appendChild(blogImg);
     let blogName = document.createElement('h2');
     blogName.innerHTML = blogs[i].title;
-    blog.appendChild(blogName);
+    click.appendChild(blogName);
     let blogDate = document.createElement('h3');
     blogDate.innerHTML=blogs[i].date;
-    blog.appendChild(blogDate);
+    click.appendChild(blogDate);
+    let blogType = document.createElement('button');
+    blogType.innerHTML = blogs[i].type;
+    blogType.addEventListener("click", function(){
+      for (let i = 0; i < blogs.length; i++){
+        let newBlog = document.getElementById('blog' + i);
+        if (newBlog.lastChild != blogType)
+          blogColumn.removeChild(newBlog);
+      }
+      let header = document.getElementById('typed');
+      header.innerHTML = blogType.innerHTML;
+      header.style.fontStyle = "italic";
+    });
+    blog.appendChild(blogType);
   }
 
 
@@ -76,7 +91,7 @@ function showBlog(index){
   blogContainer.appendChild(blogpost);
   let backButton = document.createElement('button');
   backButton.innerHTML = "<i class='fa-solid fa-backward'></i>Back";
-  backButton.onclick = function(){ changeBack()};
+  backButton.addEventListener("click", function(){changeBack()});
   blogpost.appendChild(backButton);
   let blogName = document.createElement('h1');
   blogName.innerHTML = blogs[index].title;
@@ -97,5 +112,4 @@ function changeBack(){
   blogColumn.style.display = 'flex';
 }
 
-// let navbar = document.getElementById('navbar');
 
