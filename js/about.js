126ddf150e866fb237835930f0038fb9974655aa
Zach <113458429+zach-bar@users.noreply.github.com>
2024-11-26 14:42:12 -0800

    Update about.js

--- js/about.js
+++ js/about.js
@@ -34,13 +34,10 @@ function show(shown, hidden) {
   
   let about_pics = [
     "./assets/img1.jpg",
-    "./assets/img2.jpg",
     "./assets/img3.jpg",
     "./assets/img4.jpg",
-    "./assets/img5.jpg",
     "./assets/img6.JPG",
     "./assets/img7.jpg",
-    "./assets/img8.JPG",
     "./assets/img9.jpg",
   ]
   let about = document.getElementById("self-description");
